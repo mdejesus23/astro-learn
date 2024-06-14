5bbed1087e6baf4ec21ec0456b0dee099e93bd6a
Melnard <dejesusmelnard@gmail.com>
2024-06-14 19:27:33 +0800

    add jsx component for greeting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://learn-astro-is-fun.netlify.app",
+  integrations: [preact()]
 });
